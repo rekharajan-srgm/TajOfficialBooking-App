fe7dc8ffda7576160f9e0392458e613ba298f960
rekharajan-srgm <rekharajan.srgm@gmail.com>
2025-06-05 08:15:06 +0530

    menu page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
-    {path:'about',component:AboutComponent}
+    {path:'about',component:AboutComponent},
+    {path:'menu',component:MenuComponent}
 ];
